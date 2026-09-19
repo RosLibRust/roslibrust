cfe4a393c7cd914a5537f159c89c594ee4e3afa8
Carter12s <Carter12s@users.noreply.github.com>
2026-09-19 16:39:58 +0000

    deploy: feeef29e2b6eba2e378e9cc315d4c8eb589e5a8d

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-540b4459.js',
+                path_to_root + 'searchindex-179cb50b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
